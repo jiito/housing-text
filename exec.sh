f1fdee71790363c519020cdf421d7924e44575fc
jiito <benjamin.allanrahill@gmail.com>
2022-04-21 22:08:21 -0400

    fix: removing env vars

--- exec.sh
+++ exec.sh
@@ -1,5 +1,3 @@
 cd "$(dirname "$0")";
-export TWILIO_AUTH_TOKEN=7bdc1a3bd9f0b9ec102d7adb522a639b 
-export TWILIO_ACCOUNT_SID=ACcecbc3357e144444e17986078fadb9ba
 source venv/bin/activate
 python script.py
